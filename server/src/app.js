852196d7a7b019d65cbaf3a52921af45f3d4dc35
div-mittal <113824753+div-mittal@users.noreply.github.com>
2024-08-26 21:25:07 +0530

    setup done

---
+++ server/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: "20kb"
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "20kb"
+}));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+
+export { app }
---
+++ server/src/constants.js
@@ -0,0 +1,6 @@
+export const DB_NAME = 'mydb';
+
+export const options = {
+    httpOnly: true,
+    secure: true,
+}
